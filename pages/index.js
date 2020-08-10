d1f77bb4f773314b22a5a5e1fcc3d9736ca1c1d3
nhsz <nh.quiroz@gmail.com>
2020-08-10 02:35:15 -0300

    starter setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Nav from '../components/nav';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className='py-20'>
+        <h1 className='text-5xl text-center'>Next.js + Tailwind CSS (with PurgeCSS) starter</h1>
+      </div>
+    </div>
+  );
+}
